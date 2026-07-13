78db2c98d64c34424e01052821361546d1684a4a
behzad <behzad6870@gmail.com>
2026-07-13 20:54:11 +0330

    added static directories and urls in settings
    +static root
    +static url
    +media root
    +media url

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 #from .views import http_test,json_test
 
 
@@ -26,3 +28,6 @@ urlpatterns = [
   
     
 ]
+urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
