88af38b6f6180b1b20b1acd8cb96d9475c6f7564
behzad <behzad6870@gmail.com>
2026-07-18 19:14:57 +0330

    count view of post and publish
    in test method added what can count view of post and in blog view added protocol can compare publish date with time zone now later publish post

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,get_object_or_404
+from django.utils import timezone
 from .models import Post
 
 def blog_view(requests):
-    posts=Post.objects.filter(status=1)
+    posts=Post.objects.filter(status=1,published_date__lte=timezone.now())
     context={'posts':posts}
     return render(requests,'blog/blog-home.html',context)
 
@@ -11,5 +12,8 @@ def single_block(requests):
 def test_view(requests,pid):
     #post=Post.objects.get(id=pid)
     post=get_object_or_404(Post,pk=pid)
+    if get_object_or_404(Post,pk=pid):
+        post.counted_views+=1
+        post.save()
     context={'post':post}
     return render(requests,'test.html',context)
