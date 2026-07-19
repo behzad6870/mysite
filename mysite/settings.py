2a776319067674db9bde0b16e39b4ff257d77188
behzad <behzad6870@gmail.com>
2026-07-19 11:20:57 +0330

    image fields
    added image field to Post model of blog app

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,9 +124,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "statics",
     
 ]
-#STATIC_ROOT= BASE_DIR  / 'static'
+STATIC_ROOT= BASE_DIR  / 'static'
 MEDIA_URL='/media/'
-#MEDIA_ROOT=BASE_DIR /'media'
+MEDIA_ROOT=BASE_DIR /'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
