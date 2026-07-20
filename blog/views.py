72d7453139490be1cf3b19d283709f0d764fcff0
behzad <behzad6870@gmail.com>
2026-07-20 10:26:55 +0330

    post_prev and post_next
    ساختاری ایجاد شد که در راستای اون در قسمت تک صفحه ای بلاک  پایین صقحه بشه بین پست قبلی و بعد در صورت وجود پیمایش کرد

--- blog/views.py
+++ blog/views.py
@@ -8,13 +8,26 @@ def blog_view(requests):
     return render(requests,'blog/blog-home.html',context)
 
 def single_block(requests,pid):
-    posts=Post.objects.filter(status=1)
+    posts=Post.objects.filter(status=1,published_date__lte=timezone.now())
     #post=get_object_or_404(posts,pk=pid)
     post=get_object_or_404(Post,pk=pid,status=1)
     if get_object_or_404(Post,pk=pid):
         post.counted_views+=1
         post.save()
-    context={'post':post}
+    list=[]  
+    for p in posts:
+        list.append(p.id)  
+    if get_object_or_404(Post,pk=pid):
+        if pid+1 in list:
+            post_next=Post.objects.get(id=pid+1)
+        else:
+            post_next=None    
+        if pid-1 in list:  
+            post_prev=Post.objects.get(id=pid+-1)  
+        else:
+            post_prev=None    
+                
+    context={'post':post,'post_next':post_next,'post_prev':post_prev}
     return render(requests,'blog/blog-single.html',context)
 def test_view(requests,pid):
     #post=Post.objects.get(id=pid)
