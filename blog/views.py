36312a9672133607d07a91c2c4aab6285280d748
behzad <behzad6870@gmail.com>
2026-07-22 11:45:41 +0330

    inlude template tag and  popular post widget
    use template tag ''include" for user info and popular widget and created dynamic popular post order by counted views

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,34 @@ from .models import Post
 
 def blog_view(requests):
     posts=Post.objects.filter(status=1,published_date__lte=timezone.now())
-    context={'posts':posts}
+    dict_views={}
+    for post in posts:
+        dict_views[post.id]=post.counted_views
+    sorted_dict = dict(sorted(dict_views.items(),key=lambda item: item[1],reverse=True))  
+    sorted_list=[]
+    for value in sorted_dict.keys():
+        sorted_list.append(value)
+    popular_posts=[]    
+    for i in range(4):
+        popular_posts.append(Post.objects.get(id=sorted_list[i]))
+        
+      
+    context={'posts':posts,'popular_posts':popular_posts}
     return render(requests,'blog/blog-home.html',context)
 
 def single_block(requests,pid):
     posts=Post.objects.filter(status=1,published_date__lte=timezone.now())
     #post=get_object_or_404(posts,pk=pid)
+    dict_views={}
+    for post in posts:
+        dict_views[post.id]=post.counted_views
+    sorted_dict = dict(sorted(dict_views.items(),key=lambda item: item[1],reverse=True))  
+    sorted_list=[]
+    for key in sorted_dict.keys():
+        sorted_list.append(key)
+    popular_posts=[]    
+    for i in range(4):
+        popular_posts.append(Post.objects.get(id=sorted_list[i]))
     post=get_object_or_404(Post,pk=pid,status=1)
     if get_object_or_404(Post,pk=pid):
         post.counted_views+=1
@@ -27,7 +49,7 @@ def single_block(requests,pid):
         else:
             post_prev=None    
                 
-    context={'post':post,'post_next':post_next,'post_prev':post_prev}
+    context={'post':post,'post_next':post_next,'post_prev':post_prev,'popular_posts':popular_posts}
     return render(requests,'blog/blog-single.html',context)
 def test_view(requests,pid):
     #post=Post.objects.get(id=pid)
