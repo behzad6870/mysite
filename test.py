35e85c6b10837fe21e43ceae4e2124ad9ba21968
behzad <behzad6870@gmail.com>
2026-07-22 12:18:20 +0330

    یاداوری توابع پایتونی
    map: d;  یه فانکشن رو به کل اعضای یک مجموعه که قابل شمارش هس  اعمال میکنه
    lambda

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-def test (name=None,family='mirzaei',age=0):
-    print(name,family,age)
-    return 0
+numbers=[1,2,3,4,5]
+num=[]
 
-test(age=25,name='ghkkgh')
+for item in numbers:
+        num.append(item*2)
+print(num)   
+
+def my_fun(num):
+    return num*2
+
+result1=map(my_fun,numbers)  
+print(list(result1))  
+
+result2=map(lambda x:x*3 ,numbers) 
+print(list(result2))
